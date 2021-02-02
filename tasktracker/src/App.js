97f9f87727463449b5d6d530234bfc9c354e7deb
Brandon Cermak <cermakprofessional@gmail.com>
2021-02-02 09:24:17 -0600

    breaking down tasks components/props , imported FaTimes from react-icons library

--- tasktracker/src/App.js
+++ tasktracker/src/App.js
@@ -1,12 +1,33 @@
-import Header from "./components/Header";
+import Header from "./components/Header"
 import Tasks from "./components/Tasks"
+import {useState} from 'react'
 
 const App = () => {
+  const [tasks, setTask] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30PM',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Work Meeting',
+        day: 'Feb 6th at 1:30PM',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Grocery Shopping',
+        day: 'Feb 5th at 2:30PM',
+        reminder: false,
+    },
+])
 
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
