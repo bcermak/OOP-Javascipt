1a72400806191d810655fdfb74f2366eb964da92
Brandon Cermak <cermakprofessional@gmail.com>
2021-02-02 11:15:55 -0600

    inserted addNewTask function

--- tasktracker/src/App.js
+++ tasktracker/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
+import AddTask from "./components/AddTask"
 import {useState} from 'react'
 
 const App = () => {
@@ -24,6 +25,14 @@ const App = () => {
     },
 ])
 
+// Add Task
+
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = {id,...task}
+  setTasks([...tasks, newTask])
+}
+
 // Delete Task
 
 const deleteTask = (id) => {
@@ -40,6 +49,7 @@ const toggleReminder = (id) => {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks Left!' }
     </div>
   );
