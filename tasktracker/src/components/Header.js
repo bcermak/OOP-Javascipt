ffbb5b425e5d6cbdcace2672032e7d6946ccdb0b
Brandon Cermak <cermakprofessional@gmail.com>
2021-02-04 12:52:03 -0600

    created JSON Server, added Routes, etc

--- tasktracker/src/components/Header.js
+++ tasktracker/src/components/Header.js
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types'
 import Buttons from './Buttons'
+import { useLocation } from 'react-router-dom'
 
 const Header = ({ title, onAdd, showAdd}) => {
+    const location = useLocation()
 
     return (
         <header className='header'>
-            <h1> {title}</h1>
-            <Buttons 
+            {location.pathname === '/' && <h1> {title}</h1>}
+            {location.pathname === '/' && <Buttons 
                 color={showAdd ? 'Red' : 'Green'} 
                 text={showAdd ? 'Close' : 'Add'} 
-                onClick={onAdd}/>
+                onClick={onAdd}
+            />}
         </header>
     )
 }
